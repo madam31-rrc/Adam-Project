137330e82ea357cdb9be0288bbf8a6813ed69799
madam31-rrc <madam31@academic.rrc.ca>
2024-09-06 16:21:48 -0500

    My First Program

---
+++ assignment.py
@@ -0,0 +1,14 @@
+"""
+Description: My First Program
+Author: Adam Munir
+Date:  6/09/2024
+Usage: python Hello_World.py
+"""
+# English
+print ("Hello World!" )
+
+# French 
+print("Bonjour le monde!")
+
+# Dutch
+print("Hallo Wereld!")
