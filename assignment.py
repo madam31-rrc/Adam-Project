730dfaa999022422f7d597d235bd65e864a6a6c4
madam31-rrc <madam31@academic.rrc.ca>
2024-09-07 19:39:33 -0500

    Initial commit: Set up project directory and file structure

--- assignment.py
+++ assignment.py
@@ -5,5 +5,10 @@ Date:  6/09/2024
 Usage: python Hello_World.py
 """
 # English
-print ("Hello World!" )
+print ("Hello World!")
 
+# Fench
+print ("Bonjour le mond!")
+
+# Dutch
+print ("Hello Wereld!")  
